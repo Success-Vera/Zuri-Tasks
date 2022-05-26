c9dac4b0a0780833d66574b700e84d5c670073f1
Ifeoma Veronica Nwabufo <66289160+Success-Vera@users.noreply.github.com>
2022-05-26 16:32:26 +0000

    Finding Anagrams

---
+++ main.py
@@ -0,0 +1,13 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if list(word)==list(anagram)[::-1]:
+      return True
+    else:
+      return False
+
