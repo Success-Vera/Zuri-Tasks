0ef6d91eb79d0d2b6a3bd4eba9b3e79d7922d80b
Success-Vera <ifysuccessvera@gmail.com>
2022-06-06 17:42:07 +0000

    added django project

---
+++ ifeoma123/myproject/api/serializers.py
@@ -0,0 +1,9 @@
+import imp
+from rest_framework import serializers
+from base.models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Item
+        fields='__all__'
+        
